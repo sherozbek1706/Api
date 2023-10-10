3fe1805b72fbd4848899efb3acb06a5d03e0430c
abduazim <119504866+abduazimkh@users.noreply.github.com>
2023-10-10 23:22:53 +0500

    with axios fetched

---
+++ script/sign-in.js
@@ -0,0 +1,63 @@
+const $form = document.querySelector("#form"),
+  $emailInput = $form.querySelector("#email"),
+  $passwordInput = $form.querySelector("#password"),
+  $emailMessage = $form.querySelector("[data-message-email]"),
+  $passwordMessage = $form.querySelector("[data-message-password]"),
+  $mainMessage = $form.querySelector("[data-main-message]");
+
+const EMAIL_REGEX = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,})+$/;
+const PASSWORD_REGEX = /[a-zA-Z0-9]{8,}/;
+
+$form.addEventListener("submit", createNewUser);
+
+function checkValue(value, regex, element, type) {
+  if (!regex.test(value)) {
+    element.setAttribute(`data-message-${type}`, "error");
+    element.innerText = `${type} is invalid`.toUpperCase();
+    element.style.display = "block";
+  }
+  return regex.test(value);
+}
+
+async function createNewUser(e) {
+  e.preventDefault();
+
+  const isEmailValid = checkValue(
+    $emailInput.value,
+    EMAIL_REGEX,
+    $emailMessage,
+    "email"
+  );
+  const isPasswordValid = checkValue(
+    $passwordInput.value,
+    PASSWORD_REGEX,
+    $passwordMessage,
+    "password"
+  );
+
+  if (isEmailValid && isPasswordValid) {
+    try {
+      const response = await axios.post(
+        `http://localhost:3000/api/auth/signin`,
+        {
+          email: $emailInput.value,
+          password: $passwordInput.value,
+          headers: {
+            "Content-type": "application/json",
+          },
+        }
+      );
+
+      console.log(response.data);
+      if(response.status === 200){
+        localStorage.setItem("token", response.data.token)
+        // location.replace(location.origin+"/pages/sign-in.html")
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  } else {
+    $mainMessage.setAttribute("data-main-message", "error");
+    $mainMessage.innnerText = "You intered something wrong!";
+  }
+}
